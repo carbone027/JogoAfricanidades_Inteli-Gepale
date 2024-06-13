9ca58bda1e1295539f6f79523330777113bc8a92
Davi Nascimento de Jesus <davinasc.dejesus@gmail.com>
2024-06-13 11:38:22 -0300

    Adiciona background da tela de créditos e botão

--- src/UI/credits.js
+++ src/UI/credits.js
@@ -4,71 +4,37 @@ export class credits extends Phaser.Scene {
         super ({ key: 'credits' })
     }
 
+    preload () {
+      this.load.image('backgroundCredits', '../assets/backgroundCredits.png');
+      this.load.image('homeButton', '../assets/homeButton.png');
+    }
     create () {
-        let coluna_design = this.cameras.main.width/5
-        let coluna_arte = this.cameras.main.width/2
-        let coluna_prog = 4*this.cameras.main.width/5
-        let alturaColunas = 128
-        let tamanhoTexto_titulo = '34px'
-        let tamanhoTexto_normal = '28px'
-        let corTexto_base = '#fff9f0'
 
-        const texto_creditos = this.add.text(this.cameras.main.width/2, 64, 'Créditos', { fontSize: '40px', fill: corTexto_base }).setOrigin()
+      const background = this.add.image(0, 0, 'backgroundCredits')
+      background.setOrigin(0,0);    
+      
 
-        const design = this.add.text(coluna_design, alturaColunas, 'Design', { fontSize: tamanhoTexto_titulo, fill: corTexto_base }).setOrigin()
-        const nomes_design = ['Davi', 'Isaac', 'João', 'Jonathan', 'Lavínia', 'Lívia', 'Thiago']
-        nomes_design.forEach(
-            nome => {
-                this.add.text(coluna_design, alturaColunas+42+nomes_design.indexOf(nome)*28, nome, { fontSize: tamanhoTexto_normal, fill: corTexto_base }).setOrigin()
-            }
-        )
-
-        const arte = this.add.text(coluna_arte, alturaColunas, 'Arte', { fontSize: tamanhoTexto_titulo, fill: corTexto_base }).setOrigin()
-        const nomes_arte = ['Davi', 'Jonathan', 'Thiago']
-        nomes_arte.forEach(
-            nome => {
-                this.add.text(coluna_arte, alturaColunas+42+nomes_arte.indexOf(nome)*28, nome, { fontSize: tamanhoTexto_normal, fill: corTexto_base }).setOrigin()
-            }
-        )
-
-        const programacao = this.add.text(coluna_prog, alturaColunas, 'Programação', { fontSize: tamanhoTexto_titulo, fill: corTexto_base }).setOrigin()
-        const nomes_prog = ['João']
-        nomes_prog.forEach(
-            nome => {
-                this.add.text(coluna_prog, alturaColunas+42+nomes_prog.indexOf(nome)*28, nome, { fontSize: tamanhoTexto_normal, fill: corTexto_base }).setOrigin()
-            }
-        )
-
-        const orientadores = this.add.text(coluna_prog, nomes_prog.length*28+alturaColunas+64, 'Orientadores', { fontSize: tamanhoTexto_titulo, fill: corTexto_base }).setOrigin()
-        const nomes_orientadores = ['Fabiana', 'Julia', 'Robson', 'Vinicius']
-        nomes_orientadores.forEach(
-            nome => {
-                this.add.text(coluna_prog, nomes_prog.length*28+alturaColunas+106+nomes_orientadores.indexOf(nome)*28, nome, { fontSize: tamanhoTexto_normal, fill: corTexto_base }).setOrigin()
-            }
-        )
-
-        const texto_producao = this.add.text(this.cameras.main.width/2, 512, 'Uma produção Gepale (Grupo de Estudos e Pesquisas em Política e Avaliação Educacional)\ne Inteli (Instituto de Tecnologia e Liderança)', { fontSize: '18px', fill: '#fff9f0', align: 'center' }).setOrigin()
-
-        const texto_voltar = this.add.text(this.cameras.main.width/2, 576, 'Voltar', { fontSize: tamanhoTexto_titulo, fill: corTexto_base })
-            .setOrigin()
-            .setInteractive()
-        texto_voltar
+      const homeButton = this.add.image(this.cameras.main.width/2+350, this.cameras.main.height/2+180, 'homeButton').setOrigin().setInteractive();
+        
+        homeButton
             .on('pointerover', () => {
-                texto_voltar.setStyle({ fontSize: '38px', fill: '#fcd8b6' })
+                homeButton.setScale(1.1);
             }, this)
             .on('pointerout', () => {
-                texto_voltar.setStyle({ fontSize: '34px', fill: '#fff9f0' })
+                homeButton.setScale(1.0);
             }, this)
             .on('pointerdown', () => {
-                texto_voltar.setStyle({ fontSize: '34px', fill: '#947b63' })
                 this.time.delayedCall(500, () => {
-                    this.scene.start(window.previousScene)
+                    this.scene.start('firstScene')
                 }, [], this)
-            }, this)
-            .on('pointerup', () => {
-                texto_voltar.setStyle({ fontSize: '38px', fill: '#fcd8b6' })
-            }, this)
+            }, this);
 
+            homeButton.on('pointerover', () => {
+              this.input.setDefaultCursor("pointer");
+            });
+            homeButton.on('pointerout', () => {
+              this.input.setDefaultCursor("default");
+            });
     }
 
 }
