15421c6c1bfc694de5ab5b48ff99b3dcd0bed901
Davi Nascimento de Jesus <davinasc.dejesus@gmail.com>
2024-06-12 22:19:45 -0300

    Adiciona background da tela de início e botões

--- src/UI/mainMenu.js
+++ src/UI/mainMenu.js
@@ -4,74 +4,51 @@ export class mainMenu extends Phaser.Scene {
         super ({ key: 'mainMenu' })
     }
 
+    preload() {
+      this.load.image('background', '../assets/telaInicialBackground.png');
+      this.load.image('startButton', '../assets/startButton.png');
+      this.load.image('creditos', '../assets/creditos.png');
+    }
+
     create () {
         window.previousScene = 'mainMenu'
 
-        const logo = this.add.image(this.cameras.main.width/2, 160, 'logo')
-        logo.setOrigin()       
+        const background = this.add.image(0, 0, 'background')
+        background.setOrigin(0,0);       
 
         // texto "Iniciar Jogo" e suas mecânicas de interatividade 
-        const texto_iniciarJogo = this.add.text(this.cameras.main.width/2, this.cameras.main.height/2, 'Iniciar Jogo', { fontSize: '54px', fill: '#fff9f0' })
-            .setOrigin()
-            .setInteractive()
-        texto_iniciarJogo
+        const startButton = this.add.image(this.cameras.main.width/2, this.cameras.main.height/2+120, 'startButton').setOrigin().setInteractive();
+        
+        startButton
             .on('pointerover', () => {
-                texto_iniciarJogo.setStyle({ fontSize: '58px', fill: '#fcd8b6' })
+                startButton.setScale(1.1);
             }, this)
             .on('pointerout', () => {
-                texto_iniciarJogo.setStyle({ fontSize: '54px', fill: '#fff9f0' })
+                startButton.setScale(1.0);
             }, this)
             .on('pointerdown', () => {
-                texto_iniciarJogo.setStyle({ fontSize: '54px', fill: '#947b63' })
                 this.time.delayedCall(500, () => {
                     this.scene.start('firstScene')
                 }, [], this)
-            }, this)
-            .on('pointerup', () => {
-                texto_iniciarJogo.setStyle({ fontSize: '58px', fill: '#fcd8b6' })
-            }, this)
-        
-        // texto "Opções" e suas mecânicas de interatividade
-        const texto_config = this.add.text(this.cameras.main.width/2, 400, 'Configurações', { fontSize: '54px', fill: '#fff9f0' })
-            .setOrigin()
-            .setInteractive()
-        texto_config
-            .on('pointerover', () => {
-                texto_config.setStyle({ fontSize: '58px', fill: '#fcd8b6' })
-            }, this)
-            .on('pointerout', () => {
-                texto_config.setStyle({ fontSize: '54px', fill: '#fff9f0' })
-            }, this)
-            .on('pointerdown', () => {
-                texto_config.setStyle({ fontSize: '54px', fill: '#947b63' })
-                this.time.delayedCall(500, () => {
-                    this.scene.start('config')
-                }, [], this)
-            }, this)
-            .on('pointerup', () => {
-                texto_config.setStyle({ fontSize: '58px', fill: '#fcd8b6' })
-            }, this)
+            }, this);
+  
 
         // texto "Créditos" e suas mecânicas de interatividade
-        const texto_creditos = this.add.text(this.cameras.main.width/2, 480, 'Créditos', { fontSize: '54px', fill: '#fff9f0' })
-            .setOrigin()
-            .setInteractive()
-        texto_creditos
+        const creditos = this.add.image(this.cameras.main.width/2, this.cameras.main.height/2+190, 'creditos').setOrigin().setInteractive();
+
+        creditos
             .on('pointerover', () => {
-                texto_creditos.setStyle({ fontSize: '58px', fill: '#fcd8b6' })
+              creditos.setScale(1.1);
             }, this)
             .on('pointerout', () => {
-                texto_creditos.setStyle({ fontSize: '54px', fill: '#fff9f0' })
+              creditos.setScale(1.0);
             }, this)
             .on('pointerdown', () => {
-                texto_creditos.setStyle({ fontSize: '54px', fill: '#947b63' })
+               
                 this.time.delayedCall(500, () => {
                     this.scene.start('credits')
                 }, [], this)
-            }, this)
-            .on('pointerup', () => {
-                texto_creditos.setStyle({ fontSize: '58px', fill: '#fcd8b6' })
-            }, this)
+            }, this);
     }
 
 }
